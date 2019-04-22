268caccead07edfd51502a219b01fbc5afd0cf54
Lucas Nagata <lucas.nagata@fatec.sp.gov.br>
2019-04-22 01:54:07 -0300

    Added API testing

--- src/test/java/br/com/shortener/ShortenerApplicationTests.java
+++ src/test/java/br/com/shortener/ShortenerApplicationTests.java
@@ -3,10 +3,12 @@ package br.com.shortener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ShortenerApplication.class)
+@TestPropertySource(locations="classpath:test.properties")
 public class ShortenerApplicationTests {
 
 	@Test
