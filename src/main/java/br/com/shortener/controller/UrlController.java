94ccb1e57fdcf6a92430176b535728bb75a24273
Lucas Nagata <lucas.nagata@fatec.sp.gov.br>
2019-04-22 19:57:07 -0300

    Fixed request mapping for original URL's with slashes (/)

--- src/main/java/br/com/shortener/controller/UrlController.java
+++ src/main/java/br/com/shortener/controller/UrlController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,18 +38,34 @@ public class UrlController {
 		return new ModelAndView("/url");
 	}
 	
-	@RequestMapping(path = "/url/{urlToCheck}", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
-	public @ResponseBody UrlDTO showShortenedUrl(RedirectAttributes redirectAttributes,
+	@RequestMapping(path = "/url/{urlToCheck}/**", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+	public @ResponseBody UrlDTO showShortenedUrl(HttpServletRequest request,
 			@PathVariable(name = "urlToCheck", required = true) String urlToCheck) {
 		
-		Optional<Url> optionalUrl = urlService.getByUrl(urlToCheck);
+		Optional<Url> optionalUrl;		
+		
+		String requestURL = request.getRequestURL().toString();
+		
+		String[] splittedUrl = requestURL.split("/url/" + urlToCheck + "/");
+		
+		if(splittedUrl.length > 1) {
+			optionalUrl = urlService.getByUrl(urlToCheck + splittedUrl[1]);
+		} else {
+			optionalUrl = urlService.getByUrl(urlToCheck);
+		}		
 		
 		if (optionalUrl.isPresent()) {			
 			return UrlDTO.from(optionalUrl.get());
 		}
 		
 		Url url = new Url();
-		url.setUrl(urlToCheck);
+		
+		if(splittedUrl.length > 1) {
+			url.setUrl(urlToCheck + "/" + splittedUrl[1]);
+		} else {
+			url.setUrl(urlToCheck);
+		}		
+		
 		url.setUrlUsage(Long.valueOf("0"));
 		url = urlService.save(url);
 		url.setShortened("shrt.io/" + ShortUrl.encode(url.getId().intValue()));
